bb425c49fa0b6bf3cd605565a2e243fe578392ca
steffc1 <95406244+steffc1@users.noreply.github.com>
2021-12-02 04:57:32 -0600

    Update and rename q3server.sh to OptionalAutomation/q3server.sh

--- OptionalAutomation/q3server.sh
+++ OptionalAutomation/q3server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script to start q3 server
+# script to start q3 server instead of having to type below command each time
 /usr/lib/ioquake3/ioq3ded +exec server.cfg +exec levels.cfg +exec bots.cfg
