c2b4b96713e6f9db93d474307aecdf94208422c1
steffc1 <95406244+steffc1@users.noreply.github.com>
2021-12-05 05:08:13 -0600

    Delete uploadscripts.sh

--- OptionalAutomation/uploadscripts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# upload scripts from cloud storage to vm
-
-gsutil cp gs://quake3files/scripts/q3serverstart.sh /home/quake3
-gsutil cp gs://quake3files/scripts/uploadpaks.sh /home/quake3
-gsutil cp gs://quake3files/scripts/uploadservercfgs.sh /home/quake3
-gsutil cp gs://quake3files/scripts/addexecuteperms.sh /home/quake3
