c77dfcfbd3451f4f7d081455d2f4fb246bf90a47
steffc1 <95406244+steffc1@users.noreply.github.com>
2021-12-05 05:10:08 -0600

    Delete addexecuteperms.sh

--- OptionalAutomation/addexecuteperms.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#add execute permissions to all scripts
-
-chmod +x /home/quake3/uploadservercfgs.sh
-chmod +x /home/quake3/uploadpaks.sh
-chmod +x /home/quake3/addexecuteperms.sh
-chmod +x /home/quake3/q3serverstart.sh
